0256427a3ab6e40211c1cb51c97f6c53007b97e4
pedrokpp <pedrokp@id.uff.br>
2022-05-20 00:10:00 -0300

    draw text z para tras e bird y mais random

--- main.py
+++ main.py
@@ -21,16 +21,16 @@ lume.y = background1.y
 
 bird1 = Sprite('./assets/bird1.png', frames=1)
 bird1.x = randint(0, WIDTH - bird1.width)
-bird1.y = randint(30, 50)
+bird1.y = randint(60, 300)
 
 hearts = GameImage('./assets/hearts/hearts.png')
 
 while True: # game loop
     window.set_background_color([136, 194, 246])
     background1.draw()
+    window.draw_text(f'{pontos}', 0, 0, size=54 ,font_name='./assets/fonts/Minecraft.ttf', file_path=True, centered=True, screen_width=WIDTH, screen_height=HEIGHT)
     lume.draw()
     hearts.draw()
     bird1.draw()
-    window.draw_text(f'{pontos}', 0, 0, size=54 ,font_name='./assets/fonts/Minecraft.ttf', file_path=True, centered=True, screen_width=WIDTH, screen_height=HEIGHT)
     window.update()
     window.delay(16) # delay de 16ms para deixar o fps estático em 60 FPS (1 frame a cada 16ms = 60 FPS)
